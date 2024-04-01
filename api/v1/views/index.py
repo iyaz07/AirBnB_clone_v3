2feb0b84798d83dfd95eb433bf7bc8fc55c54c9a
iyaz07 <moseseniade@gmail.com>
2024-04-01 05:42:36 -0700

    Create endpoint to retrieve number oof each objects by class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,17 +3,7 @@
 
 from api.v1.views import app_views
 from flask import jsonify
-
-
-hbnbObjects = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
-
+from models import storage
 
 @app_views.route('/status', strict_slashes=False)
 def status():
@@ -21,13 +11,14 @@ def status():
 
 
 @app_views.route('/stats', strict_slashes=False)
-def hbnbStats():
-    """hbnbStats"""
-    return_dict = {}
-    for key, value in hbnbObjects.items():
-        return_dict[key] = storage.count(value)
+def Stats():
+    """Get Stats"""
+    return_dict = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City')
+        'places': storage.count('place')
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User');
+        }
     return jsonify(return_dict)
-
-
-if __name__ == "__main__":
-    pass
