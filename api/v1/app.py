49954ae2a3fd3086e546d3618988523b37f93217
iyaz07 <moseseniade@gmail.com>
2024-04-01 20:46:55 -0700

    pdate api/v1/app.py to create a CORS instance allowing: /* for 0.0.0.0

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,13 +2,14 @@
 """sets up the Flask application for v1 of the API"""
 
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -18,9 +19,7 @@ def close_session(exception=None):
 
 @app.errorhandler(404)
 def not_found(error):
-    response = jsonify({"error": "Not found"})
-    response.status_code = 404
-    return response
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
