0a162cf57e363cc557ad54fbc670b6b9141be863
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-04-03 11:52:07 +0000

    states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -29,7 +29,8 @@ def get_state(state_id):
         return abort(404)
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     """
     Delete State by ID
